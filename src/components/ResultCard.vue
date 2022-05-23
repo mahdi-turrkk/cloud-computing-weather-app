af0a896bad527dff0854e980d85c2e6942d43aa5
mahdi-turrkk <mahdichavoshi14@gmail.com>
2022-05-24 01:37:15 +0430

    finish result component

---
+++ src/components/ResultCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card class="d-flex justify-space-between mb-6 mt-6"  max-width="1800px" flat>
+    <weather-card v-for="i in weatherData" :key="i.dt" :cityName='name' :weather="i" ></weather-card>
+  </v-card>
+</template>
+
+<script>
+import WeatherCard from "./WeatherCard.vue";
+  export default {
+    name: 'ResultCard',
+    components : {
+      WeatherCard
+    },
+    props : ['weatherData' , 'name'],
+
+    data: () => ({
+    }),
+  }
+</script>
