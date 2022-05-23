adcceb2f6b1401e81f5134c02d6ff38ba979d5e8
mahdi-turrkk <mahdichavoshi14@gmail.com>
2022-05-24 00:41:55 +0430

    finish input

---
+++ src/components/InputCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <v-card class="d-flex justify-center mb-6 mt-6" flat>
+      <h2>Welcome</h2>
+    </v-card>
+    <v-card class="d-flex justify-center mb-6 mt-6" flat>
+      <h4>please enter your city name</h4>
+      </v-card>
+    <v-card class="d-flex justify-center mb-6 mt-6" flat >
+      <v-text-field style="max-width : 300px" placeholder="City Name" v-model="cityName"></v-text-field>
+    </v-card>
+    <v-card class="d-flex justify-center mb-6 mt-6" flat >
+      <v-btn @click="getWeather" dark color="#ffbe0b">Get Weather</v-btn>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'InputCard',
+    components : {
+    },
+
+    data: () => ({
+      cityName : ""
+    }),
+    methods : {
+      getWeather(){
+        this.$emit('getWeather', this.cityName)
+      }
+    }
+  }
+</script>
