c63e5cee2a3d9ab79b1b333cf56784faaa7b8e79
mahdi-turrkk <mahdichavoshi14@gmail.com>
2022-05-24 01:37:22 +0430

    finish fill app

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,89 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+    <v-app-bar app color="#ffbe0b" dark height="50">
+      <v-btn icon @click="back">
+        <v-icon>mdi-arrow-left</v-icon>
       </v-btn>
+      <v-spacer></v-spacer>
+      <v-container>Weather App</v-container>
+      <v-spacer></v-spacer>
     </v-app-bar>
-
     <v-main>
-      <HelloWorld/>
+      <input-card v-if="isInput" @getWeather="getWeather"></input-card>
+      <result-card v-if="isResult" :name='cityName' :weatherData='weatherData'></result-card>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import ResultCard from './components/ResultCard';
+import InputCard from './components/InputCard';
+
+
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    ResultCard,
+    InputCard
   },
 
   data: () => ({
-    //
+    cityName : "",
+    isInput : true,
+    isResult : false,
+    weatherData : []
   }),
+  methods : {
+    getWeather(data){
+      let lat = ''
+      let lon = ''
+      this.cityName = data
+      fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${data}&appid=2c15dcc8b030c2e671e32d29c5483615`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          console.log("fuck")
+        }
+        }).then(coordination => {
+          lat = coordination[0].lat
+          lon = coordination[0].lon
+          return lon
+        }).then(d =>{
+          fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&cnt=50&appid=2c15dcc8b030c2e671e32d29c5483615`)
+          .then(response => {
+            console.log(d)
+            if (response.ok) {
+              return response.json();
+            } else {
+              console.log("fuck")
+            }
+            }).then(weather => {
+              weather.list.splice(1 , 7)
+              weather.list.splice(2 , 7)
+              weather.list.splice(3 , 7)
+              weather.list.splice(4 , 7)
+              weather.list.splice(5 , 7)
+              this.weatherData = weather.list
+              console.log(weather.list)
+            })
+            this.back()
+        })
+
+        
+    },
+    back(){
+      if(this.isInput == true){
+        this.isInput = false
+        this.isResult = true
+      }    
+      else{
+        this.isInput = true
+        this.isResult = false        
+      }
+    }
+  }
 };
 </script>
